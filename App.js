4570b221fb8d8f2b4f6c2c403fcb58ffe212dffe
hwhyerin <sky114j@gmail.com>
2021-11-03 05:58:11 +0900

    button 배치

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Rect from "react";
+import { Button, ScrollView, View, StyleSheet } from "react-native";
+import  constants  from "expo-constants";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <>
+          <View style={{paddingTop: constants.statusBarHeight}}>
+          <View style={[styles.container, styles.playingSpace]}>
+      
+          </View>
+          <ScrollView style={[styles.container]}>
+            <View style={[styles.controlSpace]}> 
+              <View style={[styles.buttonView]}> 
+                  <Button title="CHANGE FLEX DIRECTION"/> 
+              </View> 
+              <View style={[styles.buttonView]}> 
+                  <Button title="CHANGE JUSTIFY CONTENT"/>>
+              </View>
+              <View style={[styles.buttonView]}> 
+                  <Button title="CHANGE ALIGAN ITEMS"/>
+              </View>
+              <View style={[styles.buttonView]}> 
+                  <Button title="CHANGE DIRECTION"/>>
+              </View>
+              <View style={[styles.buttonView]}> 
+                  <Button title="CHANGE FLEX WRAP"/>>
+              </View>
+              <View style={[styles.buttonView]}> 
+                  <Button title="ADD SQUARE"/>>
+              </View>
+      </View>
+      </ScrollView>
+    </>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+container: {
+   height: '50%', 
   },
-});
+playingSpace: { 
+  backgroundColor: 'white', 
+  borderColor: 'blue', 
+  borderWidth: 3,
+},
+controlSpace: { 
+    flexDirection: 'row', 
+    flexWrap: 'wrap', 
+    backgroundColor: '#F5F5F5', 
+  },
+buttonView: { 
+    width:'50%', 
+    padding: 10, 
+    }
+
+  
+
+
+})
+
+export default App;
